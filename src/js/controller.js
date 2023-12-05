a78801cf273b27b1f11bfde50c8ac256b3e5e754
rluki99 <120097849+rluki99@users.noreply.github.com>
2023-12-05 16:12:03 +0100

    feat: implemented search results

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,14 +1,15 @@
+import { async } from 'regenerator-runtime';
 import * as model from './model.js'
 import recipeView from './views/recipeView.js'
+import searchView from './views/searchView.js'
+import resultsView from './views/resultsView.js'
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-const recipeContainer = document.querySelector('.recipe');
-
-// https://forkify-api.herokuapp.com/v2
-
-///////////////////////////////////////
+if(module.hot) {
+  module.hot.accept()
+}
 
 const controlRecipes = async function () {
   try {
@@ -29,8 +30,26 @@ const controlRecipes = async function () {
   }
 };
 
+const controlSearchResults = async function() {
+  try {
+    resultsView.renderSpinner()
+
+    // 1) get search query
+    const query = searchView.getQuery()
+    if(!query) return
+
+    // 2) load search results
+    await model.loadSearchResults(query)
+
+    // 3) redner results
+    resultsView.render(model.state.search.results)
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 const init = function() {
   recipeView.addHandlerRender(controlRecipes)
+  searchView.addHandlerSearch(controlSearchResults)
 }
 init()
--- src/js/model.js
+++ src/js/model.js
@@ -3,11 +3,15 @@ import { getJSON } from './helpers.js';
 
 export const state = {
   recipe: {},
+  search: {
+    query: '',
+    results: [],
+  },
 };
 
 export const loadRecipe = async function (id) {
   try {
-    const data = await getJSON(`${API_URL}/${id}`)
+    const data = await getJSON(`${API_URL}${id}`);
 
     const { recipe } = data.data;
     state.recipe = {
@@ -20,9 +24,28 @@ export const loadRecipe = async function (id) {
       cookingTime: recipe.cooking_time,
       ingredients: recipe.ingredients,
     };
-    console.log(state.recipe);
   } catch (error) {
-    console.error(`${error} 💥💥💥`)
-    throw error
+    console.error(`${error} 💥💥💥`);
+    throw error;
+  }
+};
+
+export const loadSearchResults = async function (query) {
+  try {
+    state.search.query = query
+
+    const data = await getJSON(`${API_URL}?search=${query}`);
+
+    state.search.results = data.data.recipes.map(rec => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        image: rec.image_url,
+      };
+    });
+  } catch (error) {
+    console.error(`${error} 💥💥💥`);
+    throw error;
   }
 };
