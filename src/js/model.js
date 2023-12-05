db7aaf40c9c1276e3ce666367de5eb270f9d20cb
rluki99 <120097849+rluki99@users.noreply.github.com>
2023-12-05 22:03:46 +0100

    feat: recipe servings updating on click

--- src/js/model.js
+++ src/js/model.js
@@ -60,3 +60,12 @@ export const getSearchResultsPage = function (page = state.search.page) {
 
   return state.search.results.slice(start, end);
 };
+
+export const updateServings = function(newServings) {
+  state.recipe.ingredients.forEach(ing => {
+    ing.quantity = ing.quantity * newServings / state.recipe.servings
+    // newQt = oldQt * newServings / oldServings
+  });
+
+  state.recipe.servings = newServings
+}
